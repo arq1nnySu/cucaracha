9b933fc4452f84b994fb54109638ce99e04447f1
susanrosito <rosito.susana@gmail.com>
2016-11-11 15:55:59 -0300

    incorporando la funcionalidad con la pila

--- src/modules/Compiler.js
+++ src/modules/Compiler.js
@@ -37,34 +37,47 @@ class Writer {
 
 Program.prototype.compile = function(){
 	let writer = new Writer() 
+	
 	writer.writeSection("data", "section .data \n")
 	writer.writeSection("text", "section .text \n")
 	writer.writeSection("text", "global main \n")
 	writer.writeSection("text", "extern exit")
-	//_putchar
+	
 	this.forEach(f => f.compile(writer))
+	
 	writer.write("main:")
-	writer.writeT( "call cuca_main")
-	writer.writeT( "mov rdi, 0")
-	writer.writeT( "call exit")
+	writer.writeT("call cuca_main")
+	writer.writeT("mov rdi, 0")
+	writer.writeT("call exit")
+	
 	return writer.build()
 }
 
 Fun.prototype.compile = function(writer){
+	var varLocal = {}
+	var spcreq = (this.parameters.length+1) * 8 // +1 es porque se guarda el valor de retorno
+	
 	if(this.id == "putChar" || this.id == "putNum") return "";
 	writer.write("cuca_"+this.id+":")
-	this.block.compile(writer)
+	writer.writeT("push rbp")
+	writer.writeT("mov rbp, rsp")
+	this.parameters.forEach(p => {
+		varLocal[p.id] = spcreq
+		spcreq = spcreq -8
+		})
+	this.block.compile(writer, varLocal)
 } 	
 
-Block.prototype.compile = function(writer){
-	this.statements.forEach(s => s.compile(writer))
+Block.prototype.compile = function(writer, varLocal){
+	this.statements.forEach(s => s.compile(writer, varLocal))
+	writer.writeT("pop rbp")
 	writer.writeT( "ret")
 }
 
-StmtCall.prototype.compile = function(writer){
+StmtCall.prototype.compile = function(writer, varLocal){
 	if(this.id == "putChar"){
 		writer.writeText(", putChar")
-		writer.writeT("mov rdi, "+this.expresions[0].value) 
+		writer.writeT("mov rdi, [rbp + "+varLocal[this.expresions[0].value]+"]") 
 		writer.writeT("call putchar")
 	}
 	if(this.id == "putNum"){
@@ -75,6 +88,21 @@ StmtCall.prototype.compile = function(writer){
 		writer.writeT("mov rax, 0")
 		writer.writeT("call printf")
 	}
+
+	if(this.id !="putNum" && this.id !="putChar"){
+      var i=0
+      var spcreq = this.expresions.length * 8 
+      writer.writeT("sub rsp, "+spcreq)
+      this.expresions.forEach(e => { e.compile(writer,i) 
+      i=i+8 })
+      writer.writeT("call cuca_"+this.id)
+      writer.writeT("add rsp, "+spcreq)
+    }
 }
+
+ExprConstNum.prototype.compile = function(writer, i){
+	writer.writeT("mov rdi, "+this.value)
+	writer.writeT("mov [rsp + "+i+"], rdi")
+	}
 
 export default {}
